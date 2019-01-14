7322f310ca2643b0bd52b9012c53c925d2a21804
Widodo Pangestu <wid.pangestu@gmail.com>
2019-01-14 09:56:11 +0700

    ch2 controlled component

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
     return (
       <div className="App">
         <form>
-          <input type="text" onChange={this.onSearchChange} />
+          <input type="text" value={searchTerm} onChange={this.onSearchChange} />
         </form>
         {list.filter(isSearched(searchTerm)).map(item => (
           <div key={item.objectID}>
