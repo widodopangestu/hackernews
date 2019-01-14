164a4d430b3e7d4ec5621ce807970fefd066c19b
Widodo Pangestu <wid.pangestu@gmail.com>
2019-01-14 09:34:50 +0700

    ch 2 updateList

--- src/App.js
+++ src/App.js
@@ -20,37 +20,24 @@ const list = [
   }
 ];
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+  onDismiss(id){
+    function isNotId(item){
+      return item.objectID !== id;
+    }
+
+    const updatedList = this.state.list.filter(isNotId);
+  }
   render() {
     return (
       <div className="App">
-        {list.map(function(item, key) {
-          return (
-            <div key={item.objectID}>
-              <span>{key}</span>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </div>
-          );
-        })}
-
-        {list.map(item => {
-          return (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </div>
-          );
-        })}
-
-        {list.map(item => (
+        {this.state.list.map(item => (
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -58,6 +45,14 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
         ))}
       </div>
