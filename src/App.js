9edb83feff7e16023aaabeef6ae79d400a9d2fca
Widodo Pangestu <wid.pangestu@gmail.com>
2019-01-14 09:58:39 +0700

    ch 2 split up components

--- src/App.js
+++ src/App.js
@@ -48,10 +48,28 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <form>
-          <input type="text" value={searchTerm} onChange={this.onSearchChange} />
-        </form>
-        {list.filter(isSearched(searchTerm)).map(item => (
+        <Search value={searchTerm} onChange={this.onSearchChange} />{" "}
+        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+      </div>
+    );
+  }
+}
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input type="text" value={value} onChange={onChange} />{" "}
+      </form>
+    );
+  }
+}
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item => (
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -60,10 +78,8 @@ class App extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button"
-              >
+              <button onClick={() => onDismiss(item.objectID)} type="button">
+                {" "}
                 Dismiss
               </button>
             </span>
@@ -73,5 +89,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
