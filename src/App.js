f5f81f655b43e954032be0467db70b65b6366edc
Widodo Pangestu <wid.pangestu@gmail.com>
2019-01-14 09:51:19 +0700

    ch 2 updateList destructure

--- src/App.js
+++ src/App.js
@@ -45,12 +45,13 @@ class App extends Component {
     this.setState({ list: updatedList });
   }
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
         <form>
           <input type="text" onChange={this.onSearchChange} />
         </form>
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => (
+        {list.filter(isSearched(searchTerm)).map(item => (
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
